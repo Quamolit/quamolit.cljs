03ca1583dbdf305d29b3c9a5867267ddc8ac2f2f
jiyinyiyong <jiyinyiyong@gmail.com>
2016-10-27 01:09:37 +0800

    use constants as the tree params

--- src/quamolit/comp/binary_tree.cljs
+++ src/quamolit/comp/binary_tree.cljs
@@ -2,24 +2,26 @@
 (ns quamolit.comp.binary-tree
   (:require [hsl.core :refer [hsl]]
             [quamolit.alias :refer [create-comp line group path]]
-            [quamolit.render.element :refer [rotate scale translate]]))
+            [quamolit.render.element :refer [rotate scale translate]]
+            [quamolit.comp.debug :refer [comp-debug]]))
 
 (declare comp-binary-tree)
 
 (declare render)
 
-(defn init-state [] [(js/Math.random) (js/Math.random) (js/Math.random) (js/Math.random)])
-
 (defn render [timestamp level]
   (fn [state mutate! instant tick]
-    (let [[r1 r2 r3 r4] state
-          x1 120
+    (let [r1 0.6
+          r2 0.9
+          r3 0.5
+          r4 0.6
+          x1 80
           y1 -220
           x1-2 10
           y1-2 -80
           x2 -140
           y2 -100
-          shift-a (* (+ 0.02 (* 0.001 r1)) (js/Math.sin (/ tick (+ (* r2 100) 800))))
+          shift-a (* (+ 0.02 (* 0.001 r1)) (js/Math.sin (/ tick (+ 800 (* r2 100)))))
           shift-b (* (+ 0.03 (* 0.001 r3)) (js/Math.sin (/ tick (+ 1300 (* 60 r4)))))]
       (group
        {}
@@ -28,19 +30,19 @@
          (translate
           {:style {:y y1, :x x1}}
           (scale
-           {:style {:ratio (+ (* 0.2 shift-a) 0.5)}}
+           {:style {:ratio (+ 0.6 (* 1.3 shift-a))}}
            (rotate
-            {:style {:angle (+ (* 3 shift-a) 10)}}
+            {:style {:angle (+ (* 30 shift-a) 10)}}
             (comp-binary-tree timestamp (dec level))))))
        (if (> level 0)
          (translate
           {:style {:y y2, :x x2}}
           (scale
-           {:style {:ratio (+ 0.83 (* 2 shift-b))}}
+           {:style {:ratio (+ 0.73 (* 2 shift-b))}}
            (rotate
-            {:style {:angle (+ (* 50 shift-b) 10)}}
+            {:style {:angle (+ (* 20 shift-b) 10)}}
             (comp-binary-tree timestamp (dec level))))))))))
 
-(def comp-binary-tree (create-comp :binary-tree init-state merge render))
+(def comp-binary-tree (create-comp :binary-tree render))
 
 (declare comp-binary-tree)
